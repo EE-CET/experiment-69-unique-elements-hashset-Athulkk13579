8115e38c29bb6d8d6ae3b421a630c01ee6fda536
Athulkk13579 <athulkrishnak8086@gmail.com>
2026-03-27 15:20:42 +0530

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,7 @@
 import java.util.LinkedHashSet;
 import java.util.Scanner;
 
-public class UniqueElements {
+public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
